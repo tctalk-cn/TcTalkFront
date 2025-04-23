ac088b28c9c70b052d609189e530b6d40c81550e
honggang.liu <honggang.liu@shopee.com>
2025-04-23 12:21:18 +0800

    代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    },
+    server: {
+        port: 3005,
+    },
 })
